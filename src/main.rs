75e5c3478121b25fa8290fde40404b53b8391bb0
Kunihiro Ishiguro <ishi@hash-set.com>
2019-11-30 13:16:07 +0900

    Preparation with tokio async/await.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-fn main() {
+use tokio::runtime::Runtime;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
         println!("Please specify BGP neighbor IP address");
         std::process::exit(1);
     }
+
+    let _rt = Runtime::new()?;
 
     let client = zebra::bgp::Client::new(&args[1]);
     if let Err(err) = client.connect() {
@@ -11,4 +15,6 @@ fn main() {
         std::process::exit(1);
     }
     println!("client connect success");
+
+    Ok(())
 }
